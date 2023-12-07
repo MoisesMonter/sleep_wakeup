d7194b7be3dd0d267f901bbb5b849f224622e2fb
Moises Monteiro <moises.monteiro2077@hotmail.com>
2023-12-07 13:32:37 -0300

    Update main

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h> // Para a função sleep
-
-#define NUM_THREADS 100 // Defina o número de threads aqui
+#include <locale.h>
+#define NUM_THREADS 100// Defina o número de threads aqui
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -11,26 +11,32 @@ int aux_out = 0;
 int aux_in = 0;
 //'''Função auxiliar da thread'''
 void* funcaoHelper(void* idThread) {
+
     long tid = (long) idThread; // Convertendo para o tipo correto
-    
+    aux_out = aux_out+1;
     printf("Thread %ld iniciada\n", tid);
-    aux_out++;
+
     // Dormir por um período aleatório entre 1 e 5 segundos
     int tempoDormindo = rand() % 5 + 1;
-    // sleep(tempoDormindo);
+    sleep(tempoDormindo);
 
     // Acordar a thread principal
+
+
     pthread_mutex_lock(&mutex);
-    aux_in++;
+    aux_in = aux_in +1;
     printf("Thread %ld dormiu por %d segundos. Acordando a thread principal.\n", tid, tempoDormindo);
     pthread_cond_signal(&cond);
     pthread_mutex_unlock(&mutex);
 
     // Finalizar com o código de retorno igual ao idThread
+
     pthread_exit(idThread);
+
 }
 
 int main(int argc, char *argv[]) {
+    setlocale(LC_ALL, "Portuguese");
     pthread_t vetorThreads[NUM_THREADS];
     int statusRetorno;
     void *statusFinalizacao;
@@ -48,6 +54,7 @@ int main(int argc, char *argv[]) {
     }
 
     //'''Aguardar até que todas as threads estejam "dormindo"'''
+
     pthread_mutex_lock(&mutex);
     printf("Thread principal aguardando todas as threads dormirem...\n");
     pthread_cond_wait(&cond, &mutex);
@@ -60,9 +67,9 @@ int main(int argc, char *argv[]) {
             printf("Erro ao executar pthread_join:%d\n", statusRetorno);
             exit(-1);
         }
-        printf("Join finalizado para a thread: %d com o código: %ld\n", i, (long)statusFinalizacao);
+        printf("Join finalizado para a thread: %d com o codigo: %ld\n", i, (long)statusFinalizacao);
     }
     printf("variavel fora sleep_wakeup: %i,\nvariavel dentro sleep_wakeup: %i;\n",aux_out,aux_in);
-    printf("Finalizando o método main\n");
+    printf("Finalizando o metodo main\n");
     pthread_exit(NULL);
 }
